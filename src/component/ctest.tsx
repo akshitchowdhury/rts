6ce987d0f3b3140ba47f4336174637a82abe4a41
Pratikshit Chowdhury <101483800+akshitchowdhury@users.noreply.github.com>
2023-06-13 11:48:06 +0530

    Add files via upload

---
+++ src/component/ctest.tsx
@@ -0,0 +1,40 @@
+import React, {Fragment,Component} from 'react'
+
+
+
+class Clash extends Component{
+
+render(){
+
+    const elem =( <div>
+        <h1> Component Test</h1>
+        
+        <p> 
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam officia nostrum labore corporis doloremque soluta est necessitatibus esse, provident porro ex doloribus at voluptas error quod voluptates neque ducimus consectetur.
+        </p>
+                </div>
+                
+                
+                
+                ) 
+                return elem;
+            
+            
+    
+}
+
+    
+    
+}
+
+
+const lem = (
+    <Fragment>
+        <Clash />
+
+        
+    </Fragment>
+);
+
+
+export default lem;
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import el from './rcss/slide'
-
+import lem from './component/ctest'
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 
 import list from './rfrag/react_frag'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  el
+  lem
 );
 
 // If you want to start measuring performance in your app, pass a function
